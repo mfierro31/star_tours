39e5b45d2679ad35eeb23b043204119073ed78ae
Mike Fierro <mfier@MacBook-Pro.local>
2020-08-26 23:08:40 -0700

    added database schema design

--- models.py
+++ models.py
@@ -15,63 +15,58 @@ class User(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
 
-    trips = db.relationship('Trip', backref='user', cascade='all, delete-orphan')
+    itinerariess = db.relationship('Itinerary', backref='user', cascade='all, delete-orphan')
 
 
 class Flight(db.Model):
-    # How would I continually update my flights on this project?  Would I just have to keep updating and running a seed file every now and then?
-    # Or is there some way to automate this?  Or should I just get rid of this model completely?
-
     __tablename__ = 'flights'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     flight_num = db.Column(db.Integer, nullable=False)
-    depart_datetime = db.Column(db.DateTime, nullable=False)
-    arrive_datetime = db.Column(db.DateTime, nullable=False)
     depart_planet = db.Column(db.Text, nullable=False)
     arrive_planet = db.Column(db.Text, nullable=False)
     depart_port = db.Column(db.Text, nullable=False)
     arrive_port = db.Column(db.Text, nullable=False)
 
-class Trip(db.Model):
-    # I was debating on adding this model.  I'm pretty sure I can set it up where I can get all of this info directly from
-    # inside my User model, but this seemed cleaner and more organized.
-    __tablename__ = 'trips'
+class Itinerary(db.Model):
+    __tablename__ = 'itineraries'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    depart_datetime = db.Column(db.DateTime, nullable=False)
+    arrive_datetime = db.Column(db.DateTime, nullable=False)
     user = db.Column(db.Text, db.ForeignKey('users.email'))
 
     # Should I add any ON DELETE CASCADEs to any of these relationships?  I feel like if I delete any planets, flights, or tours
     # this model won't be affected by that, because these aren't technically columns of data, they're just relationships
-    flights = db.relationship('Flight', secondary='trips_flights', backref='trips')
-    planets = db.relationship('Planet', secondary='trips_planets', backref='trips')
-    tours = db.relationship('Tour', secondary='trips_tours', backref='trips')
+    flights = db.relationship('Flight', secondary='itineraries_flights', backref='itineraries')
+    planets = db.relationship('Planet', secondary='itineraries_planets', backref='itineraries')
+    tours = db.relationship('Tour', secondary='itineraries_tours', backref='itineraries')
 
-# Could I do without these 3 classes below and just have done a class TripFlightPlanetTour?
+# Could I do without these 3 classes below and just have done a class ItineraryFlightPlanetTour?
 
-class TripFlight(db.Model):
+class ItineraryFlight(db.Model):
     # Do these middle tables get updated when I append info from another relationship?
-    # For instance, if I just appended flight info in a Trip object, would this table be automatically updated with the correct data?
-    __tablename__ = 'trips_flights'
+    # For instance, if I just appended flight info in a Itinerary object, would this table be automatically updated with the correct data?
+    __tablename__ = 'itineraries_flights'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     # would it be easier/make more sense to just make these things below my primary_key (both of them together)?  
     # or should I just stick to having the primary_key be a separate number id?
-    trip_id = db.Column(db.Integer, db.ForeignKey('trips.id'))
+    itinerary_id = db.Column(db.Integer, db.ForeignKey('itineraries.id'))
     flight_id = db.Column(db.Integer, db.ForeignKey('flights.id'))
 
-class TripPlanet(db.Model):
-    __tablename__ = 'trips_planets'
+class ItineraryPlanet(db.Model):
+    __tablename__ = 'itineraries_planets'
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    trip_id = db.Column(db.Integer, db.ForeignKey('trips.id'))
+    itinerary_id = db.Column(db.Integer, db.ForeignKey('itineraries.id'))
     planet = db.Column(db.Integer, db.ForeignKey('planets.name'))
 
-class TripTour(db.Model):
-    __tablename__ = 'trips_tours'
+class ItineraryTour(db.Model):
+    __tablename__ = 'itineraries_tours'
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    trip_id = db.Column(db.Integer, db.ForeignKey('trips.id'))
+    itinerary_id = db.Column(db.Integer, db.ForeignKey('itineraries.id'))
     tour_id = db.Column(db.Integer, db.ForeignKey('tours.id'))
 
 class Planet(db.Model):
